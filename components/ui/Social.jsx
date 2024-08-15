1dfaff4a2342685df5fa60923323d2d0e3086eb6
Shivansh Kaushik <shivanshkaushik1237@gmail.com>
2024-08-15 13:14:05 +0530

    social links added

--- components/ui/Social.jsx
+++ components/ui/Social.jsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 import{FaGithub, FaLinkedinIn, FaInstagram , FaTwitter} from 'react-icons/fa';
 
 const socials = [
-    { icon: <FaGithub/>, path:""},
-    { icon: <FaLinkedinIn/>, path:"" },
+    { icon: <FaGithub/>, path:"https://github.com/PhantomPhreak007"},
+    { icon: <FaLinkedinIn/>, path:"https://www.linkedin.com/in/shivansh-kaushik-b3165827a/" },
     { icon: <FaInstagram />, path:""},
     { icon: <FaTwitter/> , path:""},
 ];
