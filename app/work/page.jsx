e84a8bed26173b9d4fb9b829324b238fa0cc3260
Shivansh Kaushik <shivanshkaushik1237@gmail.com>
2024-08-26 00:30:43 +0530

    work page mobile screen alignment

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -1,7 +1,7 @@
 const Work = () => {
   return(
-  <div className="bg-primary text-white px-[138px]">
-    <span className="text-4xl">Coming Soon</span><span className="text-accent text-4xl">!</span>
+  <div className="xl:flex flex-col  bg-primary text-white xl:px-[138px] px-[30px] ">
+    <h1><span className="text-4xl">Coming Soon</span><span className="text-accent text-4xl">!</span></h1>
     <h1 className="text-white text-xl">I am curently working on a web-based <span className="text-accent w-2 ">'chat-app'</span> and soon will be uploading the project here so till then please stay tuned.</h1>
   </div>
   );
