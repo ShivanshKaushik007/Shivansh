2c210e4411f4a398ad8ffa56078f52e4f4ecb906
Shivansh Kaushik <shivanshkaushik1237@gmail.com>
2025-09-18 03:11:53 +0530

    github link update

--- components/ui/Social.jsx
+++ components/ui/Social.jsx
@@ -3,7 +3,7 @@ import{FaGithub, FaLinkedinIn, FaInstagram , FaTwitter} from 'react-icons/fa';
 import { SiLeetcode } from "react-icons/si";
 
 const socials = [
-    { icon: <FaGithub/>, path:"https://github.com/PhantomPhreak007"},
+    { icon: <FaGithub/>, path:"https://github.com/ShivanshKaushik007"},
     { icon: <FaLinkedinIn/>, path:"https://www.linkedin.com/in/shivansh-kaushik-b3165827a/" },
     { icon: <FaInstagram />, path:"https://www.instagram.com/shivanshkaushik1237"},
     { icon: <SiLeetcode/> , path:"https://leetcode.com/u/Shivansh_Kaushik/"},
