b61dd871aba76dcc99ea9cfa8ecb1d70241e3394
shivansh kaushik <shivanshkaushik1237@gmail.com>
2026-07-15 21:37:15 +0530

    services section remove

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,4 @@
 import Hero from "@/components/sections/Hero";
-import Services from "@/components/sections/Services";
 import Resume from "@/components/sections/Resume";
 import Work from "@/components/sections/Work";
 import Contact from "@/components/sections/Contact";
@@ -8,7 +7,6 @@ const Home = () => {
   return (
     <main className="flex flex-col">
       <Hero />
-      <Services />
       <Resume />
       <Work />
       <Contact />
